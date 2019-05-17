143bd72638b521877e8d1e99a0b5322a365cb228
Dhione Farias <dhione840@gmail.com>
2019-05-16 21:04:12 -0300

    Adicionando mensagem de bom dia

--- TestesGit/Program.cs
+++ TestesGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestesGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
