981e2a2ae574ddff4ec229c2e320e0f6e7708586
Dhione Farias <dhione840@gmail.com>
2019-05-19 16:43:09 -0300

    COMO VOCÊ ESTÀ

--- TestesGit/Program.cs
+++ TestesGit/Program.cs
@@ -8,6 +8,7 @@ namespace TestesGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está?");
         }
     }
 }
